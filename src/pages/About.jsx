9cab854a7e5b02836f0ddee0835bef5846904e1d
JasperKC <155926382+JasperKC@users.noreply.github.com>
2024-01-17 00:02:12 -0500

    shotgun approach

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,20 @@
-import shoebill from "../pictures/shoebill03.jpg"
+import headshot from "../pictures/jasperHeadshot.jpeg"
 import './About.css'
+import AOS from "aos";
+import "aos/dist/aos.css"
 export default function About(){
+    AOS.init({
+        duration:1500
+        });
     return(
-    <ab>
-    <h1 id = "abTitle">About</h1>
-    <div id = "abTotal">
-        <div id = "bioTotal">
+    <div class="contAb">
+        <section class ="card" id = "card0" data-aos="fade-up">
+        <img id = "hsPic" src={headshot} alt = "Headshot"></img>
+        <div>
+            <h1 id = "abTitle">About</h1>
             <p id = "abPar">Jasper Kra-Caskey is a first-year undergraduate student at Columbia University in the City of New York currently planning on double-majoring in CS and Math. Besides Math and CS, he is also academically interested in Machine/Deep Learning, Moral Philosophy, and Sustainability/Environmental Studies. Outside of school, Jasper loves to spend time outdoors rock-climbing, hiking, backpacking, and skiing. He also loves to create music, something he (briefly) explored during HS when he was a paid quartet gig singer.</p>
         </div>
-        <img id = "stork" src= {shoebill} alt="Shoebill Stork"></img>
+        </section>
     </div>
-    </ab>
     )
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,24 +1,31 @@
 import './Contact.css' 
 import columbia from "../pictures/columbia_campus.jpeg"
+import AOS from "aos";
+import "aos/dist/aos.css"
+
 export default function Contact(){
+    AOS.init({
+        duration:1000
+        });
     return(
-    <div id = "conTot">
-        {/* <h1 id = 'conTag'>How to Contact Me:</h1> */}
-        <img src={columbia} alt="Columbia Campus-view" width="100%" />
-        <div id = 'conAll'>
-            <div id = 'conTemp'>
-                <div id = 'conType'>
-                    <div id = "conSocTot">
-                        <h3 id = "socTag">Connect on social media:</h3>
-                        <p id = 'conSocPar'>To connect with Jasper on social media, you can message him on <a href="https://www.linkedin.com/in/jasper-kra-caskey-5875312a3/" target="_blank">Linkedin</a> or<a href="https://www.instagram.com/jasperkc_/" target="_blank"> Instagram</a>.</p>
-                    </div>
-                    <div id = "conEmailTot">
-                        <h3 id = "emailTag">Connect through email:</h3>
-                        <p id = 'conEmailPar'>To connect with Jasper through email, feel to reach out to him at <a href="mailto:bjk2168@columbia.edu">bjk2168@columbia.edu.</a></p>
-                    </div>
-                </div>
+    <div class="contCon">
+        <section id ="cardSec" data-aos="fade-up" >
+            <div id = "sect">
+            <form action="https://formspree.io/f/myyrzjej" method="post" id = "formCon">
+                <label for="name">Name:</label>
+                <input type="text" id="name" name="name" required></input>
+
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required></input>
+
+                <label for="message">Message:</label>
+                <textarea id="message" name="message" rows="4" required></textarea>
+
+                <button type="submit">Send Message</button>
+            </form>
             </div>
-        </div>
+                <p id = "conPar">Alternatively, consider contacting me on LinkedIn or Instagram, which are both linked in the bottom bar.</p>
+        </section>
     </div>
     )
 }
